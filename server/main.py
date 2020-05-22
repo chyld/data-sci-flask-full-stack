92ef1deff7018e35c7465e9db3dd7c5d6505b4a1
Chyld Medford <chyld.medford@gmail.com>
2020-05-21 22:22:27 -0700

    product

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, jsonify
+import numpy as np
 
 app = Flask(__name__)
 
@@ -20,3 +20,16 @@ def add(a, b):
 @app.route('/fun', methods=['GET'])
 def fun():
     return render_template('fun.html')
+
+# 5 - send list of numbers
+@app.route('/product', methods=['GET'])
+def product_get():
+    return render_template('product.html')
+
+# 6 - get product of list numbers
+@app.route('/product', methods=['POST'])
+def product_post():
+    nums = [int(s) for s in request.form['nums'].split(',')]
+    nums = np.array(nums)
+    prod = nums.prod()
+    return render_template('product.html', prod=prod)
