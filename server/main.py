3a713b50ebc789d5c5820752b631fdc83cbc57a5
Chyld Medford <chyld.medford@gmail.com>
2020-05-21 21:24:45 -0700

    fun

--- server/main.py
+++ server/main.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/hello')
+# 1 - basic GET
+@app.route('/hello', methods=['GET'])
 def hello():
     return 'Hello, World'
+
+# 2 - GET with PARAMS
+@app.route('/add/<int:a>/<int:b>', methods=['GET'])
+def add(a, b):
+    return "Sum: {}".format(a + b)
+
+# 3 - STATIC files
+
+# 4 - basic templates
+@app.route('/fun', methods=['GET'])
+def fun():
+    return render_template('fun.html')
